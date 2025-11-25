75e9ade04174635b5e5e2c47068744896782aa94
Maciej <metro1568@gmail.com>
2025-11-25 14:09:09 +0100

    Solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,170 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  beforeEach(() => {
+import {
+  chooseRandomFromArray,
+  listOfRedColor,
+  convertRGBTo255,
+  basicRedShades
+} from '../support/e2e';
 
+describe('Sign In page', {
+  viewportHeight: 1000,
+  viewportWidth: 1400,
+  scrollBehavior: 'bottom'
+},
+() => {
+  const validUsername = 'tomsmith';
+  const validPassword = 'SuperSecretPassword!';
+
+  it('should provide an ability to log in', () => {
+    const runOption = Math.floor(Math.random() + 0.5); // log in by label or not
+    let usernameField;
+    let passwordField;
+    let usernameLabel;
+    let passwordLabel;
+
+    cy.visit('/login')
+      .then(() => cy.document())
+      .then((doc) => {
+        usernameField = doc.querySelector('input[id="username"]');
+        passwordField = doc.querySelector('input[id="password"]');
+
+        if (usernameField) {
+          usernameLabel = doc.querySelector(`label[for="${usernameField.name}"]`);
+        }
+        if (passwordField) {
+          passwordLabel = doc.querySelector(`label[for="${passwordField.name}"]`);
+        }
+
+        return doc;
+      })
+      .then((doc) => {
+        if (runOption === 0 && usernameLabel && passwordLabel) {
+          cy.get(usernameLabel)
+            .type(`${validUsername}`);
+          cy.get(passwordLabel)
+            .type(`${validPassword}`);
+          cy.get('button[type="submit"]')
+            .contains('Login')
+            .click();
+        } else {
+          cy.get(usernameField)
+            .type(`${validUsername}`);
+          cy.get(passwordField)
+            .type(`${validPassword}`);
+          cy.get('button[type="submit"]')
+            .contains('Login')
+            .click();
+        }
+
+        cy.location('href')
+          .should('equal', 'https://the-internet.herokuapp.com/secure');
+      });
   });
 
-  it('', () => {
+  it(`should not allowed to log in when credential are ` +
+    `inconsistent with requirements`, () => {
+    const runOption = Math.floor(Math.random() + 0.5);
+    let usernameField;
+    let passwordField;
+    let usernameLabel;
+    let passwordLabel;
 
+    cy.visit('/login')
+      .then(() => cy.document())
+      .then((doc) => {
+        usernameField = doc.querySelector('input[id="username"]');
+        passwordField = doc.querySelector('input[id="password"]');
+
+        if (usernameField) {
+          usernameLabel = doc.querySelector(`label[for="${usernameField.name}"]`);
+        }
+        if (passwordField) {
+          passwordLabel = doc.querySelector(`label[for="${passwordField.name}"]`);
+        }
+
+        return doc;
+      })
+      .then((doc) => {
+        const invalidUsername =
+          chooseRandomFromArray(['boLekKolek32', 'Mini%43', 'mmmKuet*##']);
+        const invalidPassword =
+          chooseRandomFromArray(['brrrBUM!', 'kocccccs)99', 'HsdWWWDDPP']);
+
+        if (runOption === 0 && usernameLabel && passwordLabel) {
+          cy.get(usernameLabel)
+            .type(`${invalidUsername}`);
+          cy.get(passwordLabel)
+            .type(`${invalidPassword}`);
+          cy.get('button[type="submit"]')
+            .contains('Login')
+            .click();
+        } else {
+          cy.get(usernameField)
+            .type(`${invalidUsername}`);
+          cy.get(passwordField)
+            .type(`${invalidPassword}`);
+          cy.get('button[type="submit"]')
+            .contains('Login')
+            .click();
+        }
+
+        cy.location('href')
+          .should('equal', 'https://the-internet.herokuapp.com/login');
+
+        cy.get('[class="flash error"]')
+          .should('be.visible')
+          .then((error) => {
+            const element = error[0];
+            const elementStyle = getComputedStyle(element);
+            const elementText = element.textContent;
+            const backgroundColor = elementStyle.backgroundColor;
+
+            if (backgroundColor.startsWith('rgb')) {
+              const colorComponents = backgroundColor.match(/\d+/g);
+              const newRGBFormat = convertRGBTo255(
+                +colorComponents[0],
+                +colorComponents[1],
+                +colorComponents[2]
+              );
+              const [r, g, b] = newRGBFormat;
+              const haveRedVariant = basicRedShades.some((redVariant) => {
+                const [r2, g2, b2] = redVariant;
+
+                return r === r2 && g === g2 && b === b2;
+              });
+
+              // eslint-disable-next-line no-unused-expressions
+              expect(haveRedVariant, 'Error message has red background color')
+                .to.be.true;
+            } else {
+              expect(backgroundColor, 'Error message has red background color')
+                .to.be.oneOf(listOfRedColor);
+            }
+
+            expect(elementText, 'Error message has text (at least 2 words with 2+ letters)').to.match(/(\b\w{2,}\b ){2,}/);
+          });
+      });
+  });
+
+  it('should provide an ability to log out', () => {
+    cy.visit({
+      method: 'POST',
+      url: 'https://the-internet.herokuapp.com/authenticate',
+      body: {
+        username: validUsername,
+        password: validPassword
+      }
+    })
+      .then(() => {
+        cy.get('[href*="/logout"]')
+          .contains('Logout')
+          .click();
+
+        return cy.location('href');
+      })
+      .then((location) => {
+        expect(location).to.equal('https://the-internet.herokuapp.com/login');
+      });
   });
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,6 +16,104 @@
 // Import commands.js using ES2015 syntax:
 import './commands';
 
+// eslint-disable-next-line max-len
+const listOfRedColor = 'Blood Red  Brick Red  Bright Red  Brown  Burgundy  Burnt Umber  Burnt Orange  Burnt Sienna  Byzantium  Cadmium Red  Cardinal Red  Carmine  Cerise  Cherry  Chestnut  Claret  Coral Pink  Cordovan  Crimson  Dark Red  Falu Red  Garnet  Mahogany  Maroon  Marsala  Mulberry  Neon Red  Oxblood  Pastel Red  Persimmon  Poppy  Puce  Raspberry  Red  Red Brown  Red Ochre  Red Orange  Red Purple  Rose Red  Ruby Red  Russet  Salmon  Scarlet  Sunset Orange  Terra Cotta  Tuscan Red  Tyrian Purple  Venetian Red  Vermillion  Wine'
+  .split('  ');
+const basicRedShades = [
+  [255, 0, 64],
+  [255, 0, 43],
+  [255, 0, 21],
+  [255, 0, 0],
+  [255, 21, 0],
+  [255, 43, 0],
+  [255, 77, 77],
+  [255, 51, 51],
+  [255, 26, 26],
+  [255, 0, 0],
+  [230, 0, 0],
+  [204, 0, 0],
+  [179, 0, 0],
+  [198, 15, 19],
+  [151, 11, 14]
+];
+
+/**
+ *
+ * @param {array} array
+ * @param {number} howMany
+ * @returns {array}
+ */
+function chooseRandomFromArray(array, howMany = 1) {
+  const arrayCopy = [...array];
+
+  switch (true) {
+    case !Array.isArray(array):
+      throw new Error('"Array" parameter require an Array');
+    case array.length < 1:
+      throw new Error('"Array" parameter require min one element');
+    case typeof howMany !== 'number':
+      throw new Error('"howMany" parameter require dataType === number');
+    case howMany < 1:
+      throw new Error('"howMany" argument must be greater than 0');
+  }
+  const elementsToProcess = (howMany > array.length)
+    ? array.length
+    : howMany;
+  const result = [];
+
+  for (let i = 1; i <= elementsToProcess; i++) {
+    const elementIndex = Math.round(Math.random() * (arrayCopy.length - 1));
+    const element = arrayCopy.splice(elementIndex, 1);
+
+    result.push(element.at(0));
+  }
+
+  if (howMany === 1) {
+    return result[0];
+  }
+
+  return result;
+}
+
+/**
+ *
+ * @param {number} r /0 - 255
+ * @param {number} g /0 - 255
+ * @param {number} b /0 - 255
+ * @returns {array: {number, number, number} }
+ */
+
+function convertRGBTo255 (r = 0, g = 0, b = 0) {
+  const toReturn = [];
+  const validRGB = [];
+  const rgb = [r, g, b];
+
+  rgb.forEach((color, i) => {
+    switch (true) {
+      case color >= 0 && color <= 255:
+        validRGB[i] = color;
+        break;
+      case color < 0:
+        validRGB[i] = 0;
+        break;
+      case color > 255:
+        validRGB[i] = 255;
+        break;
+    }
+  });
+
+  for (let i = 0; i < 3; i++) {
+    const colorValue = validRGB[i];
+    const convertedValue = (!Number.isInteger(colorValue))
+      ? Math.round(colorValue * 255)
+      : colorValue;
+
+    toReturn.push(convertedValue);
+  }
+
+  return toReturn;
+}
+
 const app = window.top;
 if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
   const style = app.document.createElement('style');
@@ -26,5 +124,11 @@ if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
   app.document.head.appendChild(style);
 };
 
+export {
+  chooseRandomFromArray,
+  listOfRedColor,
+  convertRGBTo255,
+  basicRedShades
+};
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
